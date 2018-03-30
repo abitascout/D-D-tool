96503bf2920917143d43eda662c19659a69c7c22
abitascout <37875201+abitascout@users.noreply.github.com>
2018-03-29 21:53:43 -0500

    Add files via upload

---
+++ dice rolls.py
@@ -0,0 +1,40 @@
+from random import randint
+class roll():
+    def _4sided_dice_roll():
+        return randint (1,4)
+    def _6sided_dice_roll():
+        return randint (1,6)
+    def _8sided_dice_roll():
+        return randint (1,8)
+    def _10sided_dice_roll():
+        return randint (1,10)
+    def _20sided_dice_roll():
+        return randint (1,20)
+    d4 = _4sided_dice_roll()
+    d6 = _6sided_dice_roll()
+    d8 = _8sided_dice_roll()
+    d10 = _10sided_dice_roll()
+    d20 = _20sided_dice_roll()
+
+    type_of_dice = input ("What kind of dice do you want to roll?")
+    num_rolls = input ("How many rolls would you like to simulate?")
+
+
+    if type_of_dice == d6:
+        for i in range (0, num_rolls):
+            print _6sided_dice_roll()
+            
+    elif type_of_dice == d4:
+        for i in range (0,num_rolls):
+            print _4sided_dice_roll()
+    elif type_of_dice == d8:
+        for i in range (0,num_rolls):
+            print _8sided_dice_roll()
+    elif type_of_dice ==d10:
+        for i in range (0,num_rolls):
+            print _10sided_dice_roll()
+    elif type_of_dice == d20:
+        for i in range (0,num_rolls):
+            print _20sided_dice_roll()
+    else:
+        print"Invaild. Try using d4, d6, d8, d10, d20 instead."
